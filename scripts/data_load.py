063cd7dcfcb74e8f64331f8fec1b68dec903bbff
Douglas Fairfield <douglasfairfield@mac.lan>
2025-02-20 11:20:57 +0000

    script for group_by_year_mean_score_diff_mean_total_points created and imported

--- scripts/data_load.py
+++ scripts/data_load.py
@@ -1,3 +1,14 @@
 def date_to_year(df):
     df['year'] = df['date'].str[:4]
     return df
+
+def group_by_year_mean_score_diff_mean_total_points(df):
+    df = df.groupby('year').agg(
+        {
+            'score_difference': 'mean', 
+            'total_points': 'mean'
+        }).reset_index()
+    
+    df['score_difference'] = df['score_difference'].round(decimals=2)
+    df['total_points'] = df['total_points'].round(decimals=2)
+    return df
