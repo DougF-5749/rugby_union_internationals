7abc383afe3a220db2f21d0d20736e845f5bb4c4
Douglas Fairfield <douglasfairfield@mac.lan>
2025-02-20 12:08:41 +0000

    sd_tp_filter script added and imported. excitemnt for moden ear and 97-92 calculated

--- scripts/data_load.py
+++ scripts/data_load.py
@@ -12,3 +12,10 @@ def group_by_year_mean_score_diff_mean_total_points(df):
     df['score_difference'] = df['score_difference'].round(decimals=2)
     df['total_points'] = df['total_points'].round(decimals=2)
     return df
+
+def sd_tp_filter(df, one_score_val, no_dg_value, high_scoring_val):
+    one_score_mask = (df['score_difference'] < one_score_val) | (df['score_difference'] > -one_score_val)
+    no_dg_mask = (df['score_difference'] > no_dg_value) | (df['score_difference'] < -no_dg_value)
+    high_scoring_mask = df['total_points'] > high_scoring_val
+
+    return df[one_score_mask & no_dg_mask & high_scoring_mask]
